a0bb6f49845480843351b90d40660a3e0332daf5
RJField <field.robin@gmail.com>
2026-07-24 19:15:19 -0700

    added placeholder assets for weapons (sniper and basepistol) and determined hardcoded offset (ick).

--- src/weapons/WeaponDef.gd
+++ src/weapons/WeaponDef.gd
@@ -11,7 +11,7 @@ class_name WeaponDef extends Resource
 
 #firing
 @export var projectile : PackedScene #this will need to change as we introduce non projectile based weapons (beams, aoe zones, hitscans)
-@export var muzzle_offset: Vector2 
+@export var muzzle_offset: Vector2 #this will need some work, and on that note we need to also look at weapon offsets and positioning eventually.
 
 #capacity
 @export var max_mod_count : int
